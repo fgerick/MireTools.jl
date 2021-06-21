1b1302d15293fb65e49b04a3c33e9fcfac39b51f
Felix <felixgerick@gmail.com>
2021-06-21 17:38:48 +0200

    inner prod

--- src/basistools.jl
+++ src/basistools.jl
@@ -1,12 +1,10 @@
-function normbasis!(v::Vector{Mire.vptype{T1}},cmat::Array{T2,3}; n_cache=2*10^5) where {T1,T2}
-    T = promote_type(T1,T2)
-    nt = Threads.nthreads()
-    ptemp = [zeros(Mire.Term{T,Mire.Monomial{(x, y, z),3}},n_cache) for i=1:nt]
+function normbasis!(ptemp, v,cmat; n_cache=2*10^5)
+    
     n = length(v)
     u = zeros(T,n)
-    cm = T.(cmat)
+    # cm = T.(cmat)
     Threads.@threads for k=1:n
-        u[k] = sqrt(Mire.inner_product!(ptemp[Threads.threadid()],v[k],v[k],cm))
+        u[k] = sqrt(Mire._inner_product!(ptemp[Threads.threadid()],v[k],v[k],cmat))
         v[k]/=u[k]
     end
     return u
@@ -19,11 +17,15 @@ end
 function normbasis!(P::T; n_cache=2*10^6) where T<:MHDProblem
     bs1 = copy(P.bbasis.el)
     map(remove_factor!,bs1)
-    normb = normbasis!(bs1,P.cmat; n_cache)
+    nt = Threads.nthreads()
+    T3 = promote_type(T1,T2)
+    ptemp = [zeros(Mire.Term{T3,Mire.Monomial{(x, y, z),3}},n_cache) for i=1:nt]
+
+    normb = normbasis!(ptemp,bs1,P.cmat; n_cache)
     P.bbasis = typeof(P.bbasis)(P.N,P.V, bs1, P.bbasis.orthonorm)
 
     vs1 = copy(P.vbasis.el) 
-    normu = normbasis!(vs1,P.cmat; n_cache)
+    normu = normbasis!(ptemp,vs1,P.cmat; n_cache)
     P.vbasis = typeof(P.vbasis)(P.N,P.V, vs1, P.vbasis.orthonorm)
  
     return normu, normb
